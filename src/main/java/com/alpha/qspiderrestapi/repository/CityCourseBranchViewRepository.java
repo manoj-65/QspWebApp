5df7803743e6c648c0789b371f3478251ae0de15
Hrishikeshdeyofficial <101181680+Hrishikeshdeyofficial@users.noreply.github.com>
2024-07-01 10:21:12 +0530

    refact: modified review repository and course service impl API

--- src/main/java/com/alpha/qspiderrestapi/repository/CityCourseBranchViewRepository.java
+++ src/main/java/com/alpha/qspiderrestapi/repository/CityCourseBranchViewRepository.java
@@ -10,13 +10,10 @@ import com.alpha.qspiderrestapi.entity.CityCourseBranchView;
 
 public interface CityCourseBranchViewRepository extends JpaRepository<CityCourseBranchView, Long> {
 
-	@Query(value = "select ccbv.branch_id,\r\n" + "ccbv.branch_image,\r\n" + "ccbv.display_name,\r\n"
-			+ "ccbv.contacts,\r\n" + "ccbv.location,\r\n" + "ccbv.upcoming_batches, \r\n" + "ccbv.ongoing_batches,\r\n"
-			+ "b.branch_type,\r\n" + "a.city ,\r\n" + "a.pincode,\r\n" + "a.state,\r\n" + "a.street\r\n"
-			+ "from city_course_branch_view ccbv, \r\n" + "address a, \r\n" + "branch b \r\n"
-			+ "where a.city = ccbv.city\r\n" + "group by a.city, a.pincode, a.state, a.street,\r\n"
-			+ "ccbv.branch_id,\r\n" + "ccbv.branch_image,\r\n" + "ccbv.display_name,\r\n" + "ccbv.contacts,\r\n"
-			+ "ccbv.location,\r\n" + "ccbv.upcoming_batches,\r\n" + "b.branch_type,\r\n" + "ccbv.ongoing_batches;"
-			+ "", nativeQuery = true)
+	@Query(value = "\r\n" + "	\r\n"
+			+ "		select ccbv.branch_id, ccbv.branch_image, ccbv.display_name, ccbv.contacts, ccbv.location, ccbv.upcoming_batches, ccbv.ongoing_batches,\r\n"
+			+ "			b.branch_type, a.city , a.pincode, a.state, a.street from city_course_branch_view ccbv, address a, branch b where a.city = ccbv.city and b.branch_id = b.address_id group by a.city, a.pincode, a.state, a.street,\r\n"
+			+ "			ccbv.branch_id, ccbv.branch_image, ccbv.display_name, ccbv.contacts, ccbv.location, ccbv.upcoming_batches, b.branch_type, ccbv.ongoing_batches;\r\n"
+			+ "			\r\n", nativeQuery = true)
 	List<Map<String, Object>> findAllByCityName();
 }
